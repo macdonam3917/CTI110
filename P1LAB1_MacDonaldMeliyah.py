0910bb9c8c807b5237f014694754679ac3ac57c8
macdonam3917 <macdonam3917@student.faytechcc.edu>
2025-06-16 22:27:11 -0400

    FIrst submission (P1LAB1)

---
+++ P1LAB1_MacDonaldMeliyah.py
@@ -0,0 +1,9 @@
+#Meliyah MacDonald
+#6/15/25
+#P1LAB1
+#create a program that allows the user to enter first & last name.
+
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+print("Hello,", first_name, last_name, "!", "Welcome to CTI-110!")
