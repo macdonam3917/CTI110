19133acffcc11167645a880ecc68413feb397dd3
macdonam3917 <macdonam3917@student.faytechcc.edu>
2025-06-16 23:35:42 -0400

    Integers

---
+++ P1HW1_MacDonaldMeliyah.py
@@ -0,0 +1,27 @@
+#Meliyah MacDonald
+#6/16/25
+#P1WH1
+#Integers
+
+print("-----Calculating Exponenets----\n")
+
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+
+result = base ** exponent
+
+print()
+print(base, "raised to the power of", exponent, "is", result, "!!")
+
+# -----Addition and Subtraction----
+
+print("\n-----Addition and Subtraction----\n")
+
+start = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+subtract = int(input("Enter an integer to subtract: "))
+
+final_result = start + add - subtract
+
+print()
+print(start, "+", add, "-", subtract, "is equal to", final_result)
